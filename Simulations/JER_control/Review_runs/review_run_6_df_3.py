45f5d58b26a59bf8c6d616a07a79c292d65c7884
Samuel Davenport <sdavenport@Samuels-MacBook-Pro.local>
2024-05-23 18:24:00 +0200

    backup

--- Simulations/JER_control/Review_runs/review_run_6_df_3.py
+++ Simulations/JER_control/Review_runs/review_run_6_df_3.py
@@ -2,4 +2,4 @@ import sys
 sys.path.insert(0, '/home/mind/sdavenpo/2023_glmfdp/Simulations/JER_control/')
 from EJER_runs import run_nsubj_dim_review
 run_nsubj_dim_review(6, 100, simtype = 1, niters = 5000, df=3)
-run_nsubj_dim_review(6, 100, simtype = 0, niters = 5000, df=3)
+run_nsubj_dim_review(6, 100, simtype = -1, niters = 5000, df=3)
