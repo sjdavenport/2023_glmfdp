dfebea1b13b6494f4c1f4afa04625009915ecdbb
Samuel Davenport <sdavenport@Samuels-MacBook-Pro.local>
2024-05-23 17:55:20 +0200

    backup

---
+++ Simulations/JER_control/Review_runs/review_run_9_df_5.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.insert(0, '/home/mind/sdavenpo/2023_glmfdp/Simulations/JER_control/EJER_runs')
+from EJER_runs import run_nsubj_dim_review
+run_nsubj_dim_review(9, 100, simtype = 1, niters = 5000, df=5)
+run_nsubj_dim_review(9, 100, simtype = 0, niters = 5000, df=5)
---
+++ Simulations/Review_results/place.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 23 17:55:06 2024
+
+@author: sdavenport
+"""
+
